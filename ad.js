601c85271b46ec397a4b156da4334cb2ecdb3ec3
Aadarsh557 <aadarshmallila@gmail.com>
2023-11-23 11:05:15 +0530

    update

---
+++ ad.js
@@ -0,0 +1,16 @@
+let body=document.body
+let para=document.createElement('div')
+body.appendChild(para)
+para.innerHTML='PURNA MARLEY'
+console.log('para')
+para.style.backgroundColor='blue'
+para.style.textAlign='center'
+para.style.color='white'
+// 
+let para2=document.createElement('p')
+para.appendChild(para2)
+para2.innerHTML='IMRAN KHAN'
+console.log('para2')
+para2.style.fontSize='30px'
+para2.style.color='yellow'
+console.log(body)
